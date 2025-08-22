1b38758ed3b1197952b63d80d9085dade89ff666
Innocent boy <nomanriaz7980@gmail.com>
2025-08-22 13:07:58 -0700

    fix:logo error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa";
-import logo from "../assets/logs/logo.png";
+import logo from "../assets/mainlogo/logoicon.png";
 
 export default function Footer() {
   return (
